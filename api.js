6bcc740b2a05e184a5a144ee79c855af580b51a1
ZuevIvan <zueviv4@gmail.com>
2023-05-09 17:06:08 +0300

    Test first

---
+++ api.js
@@ -0,0 +1,33 @@
+export function getComments() {
+    const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/ivan-zuev/comments', {
+      method: "GET",
+    });
+  
+    return fetchPromise.then((response) => {
+      const jsonPromise = response.json();
+      return jsonPromise.then((responseData) => {
+        return responseData.comments;
+      });
+    });
+  }
+  
+  export function addNewElToList(name, text) {
+    const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/ivan-zuev/comments', {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        text
+      }),
+    });
+  
+    return fetchPromise.then((response) => {
+      if (response.status === 500 ){
+        return Promise.reject("Сервер упал");
+      } else if (response.status === 400 ){
+        return Promise.reject("Объект с ошибкой в формате");
+      } else {
+        return response.json();
+      }
+    });
+  }
+  
---
+++ render.js
@@ -0,0 +1,52 @@
+import { getComments, addNewElToList } from './api.js';
+
+const buttonElement = document.getElementById("add-button");
+const commentsElement = document.getElementById("comments");
+const nameInputElement = document.getElementById("name-input");
+const textInputElement = document.getElementById("text-input");
+
+// опрделение даты
+function getDate(){
+  const dateNow = new Date();
+  const dateNumber = String(dateNow.getDate()).padStart(2, '0');
+  const dateMonth = String(dateNow.getMonth() + 1).padStart(2, '0');
+  const dateYear =dateNow.getFullYear()
+  const dateHours = dateNow.getHours()
+  const dateMinutes = dateNow.getMinutes()
+
+  return dateNumber + "." + dateMonth +"."+ dateYear + " "+ dateHours +":"+dateMinutes;
+}
+
+// добавление нового коммента
+function addNewComment() {
+  nameInputElement.classList.remove("error");
+  textInputElement.classList.remove("error");
+
+  if (nameInputElement.value === "") {
+    nameInputElement.classList.add("error");
+    return;
+  }
+
+  if (textInputElement.value === "") {
+    textInputElement.classList.add("error");
+    return;
+  }
+
+  const name = nameInputElement.value.replaceAll("<", "&lt;").replaceAll("<", "&lt;");
+  const text = textInputElement.value.replaceAll("<", "&lt;").replaceAll("<", "&lt;");
+
+  addNewElToList(name, text).then(() => {
+    renderComments();
+    nameInputElement.value = "";
+    textInputElement.value = "";
+    nameInputElement.placeholder = "Введите ваше имя";
+    textInputElement.placeholder = "Введите ваш комментарий";
+  }).catch((error) => {
+        buttonElement.disabled = false;
+        buttonElement.textContent = 'Написать';
+        alert('Кажется что-то пошло не так, попробуй позже')
+    });
+}
+
+
+
